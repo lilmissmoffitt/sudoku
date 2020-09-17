c95aee05b73a7f3a270590dbb7c6b573d1f22e54
lilmissmoffitt <lilmissmoffitt7@gmail.com>
2020-09-16 23:11:53 -0600

    add grid param size

--- grid.js
+++ grid.js
@@ -1,21 +1,19 @@
 function showGrid() {
   let gridDiv = document.getElementById("gridDiv");
-  gridDiv.innerHTML = genGrid();
+  gridDiv.innerHTML = genGrid(9, 9);
 }
 
-function genGrid() {
+function genGrid(row, column) {
   let html = "";
-  let i = 0;
-  let j = 0;
 
   html += "<table id='game'>";
-  for (i = 0; i < 9; i++) {
+  for (i = 0; i < row; i++) {
     if ((i + 1) % 3 == 0){
       html += "<tr class='bottom-bold'>";
     } else {
       html += "<tr>";
     }
-    for(j = 0; j < 9; j++){
+    for(j = 0; j < column; j++){
       if ((j + 1) % 3 == 0 && j != 8){
         html += "<td class='right-bold'>";
         html += "</td>";
