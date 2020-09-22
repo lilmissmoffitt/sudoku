4addae7c520865017e4a6cdb609a2427d86951e3
lilmissmoffitt <lilmissmoffitt7@gmail.com>
2020-09-21 22:00:07 -0600

    add createMethod JS

--- grid.js
+++ grid.js
@@ -1,29 +1,64 @@
-function showGrid() {
+//Using innerHTML method
+// function showGrid() {
+//   let gridDiv = document.getElementById("gridDiv");
+//   gridDiv.innerHTML = genGrid(9, 9);
+// }
+
+// function genGrid(row, column) {
+//   let html = "";
+
+//   html += "<table id='game'>";
+//   for (i = 0; i < row; i++) {
+//     if ((i + 1) % 3 == 0){
+//       html += "<tr class='bottom-bold'>";
+//     } else {
+//       html += "<tr>";
+//     }
+//     for(j = 0; j < column; j++){
+//       if ((j + 1) % 3 == 0 && j != 8){
+//         html += "<td class='right-bold'>";
+//         html += "</td>";
+//       } else {
+//         html += "<td>";
+//         html += "</td>";
+//       }
+//     }
+//     html += "</tr>";
+//   }
+//   html += "</table>";
+//   return html;
+// }
+
+//Using document.createElement method
+function showGrid(){
   let gridDiv = document.getElementById("gridDiv");
-  gridDiv.innerHTML = genGrid(9, 9);
+  genGrid(gridDiv, 9, 9);
 }
 
-function genGrid(row, column) {
-  let html = "";
-
-  html += "<table id='game'>";
+function genGrid(gridDiv, row, column){
+  let newRow;
+  let newCol;
+  let table = document.createElement("table");
+  table.setAttribute("id", "game");
   for (i = 0; i < row; i++) {
     if ((i + 1) % 3 == 0){
-      html += "<tr class='bottom-bold'>";
+      newRow = document.createElement("tr");
+      newRow.className = "bottom-bold";
+      table.appendChild(newRow);
     } else {
-      html += "<tr>";
+      newRow = document.createElement("tr");
+      table.appendChild(newRow);
     }
     for(j = 0; j < column; j++){
       if ((j + 1) % 3 == 0 && j != 8){
-        html += "<td class='right-bold'>";
-        html += "</td>";
-      } else {
-        html += "<td>";
-        html += "</td>";
+        newCol = document.createElement("td");
+        newCol.className = "right-bold";
+        table.appendChild(newCol);
+        } else {
+          newCol = document.createElement("td");
+          table.appendChild(newCol);
       }
     }
-    html += "</tr>";
   }
-  html += "</table>";
-  return html;
+  gridDiv.appendChild(table);
 }
