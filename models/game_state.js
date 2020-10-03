7a8a69d8f76c3c3b4b7c64f62d7386b1438fc751
lilmissmoffitt <lilmissmoffitt7@gmail.com>
2020-10-03 15:03:21 -0600

    add settings and sample game state pages

---
+++ models/game_state.js
@@ -0,0 +1,18 @@
+function startGame () {
+  //generate game board based on difficulty selected
+  //reset hints
+  //reset mistakes
+  //start timer
+}
+
+function endGame() {
+  //stop timer
+}
+
+function pauseGame() {
+  //stop timer
+}
+
+function resumeGame() {
+  //start timer
+}
